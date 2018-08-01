adb9a221c37533bd6a712b6253ff867a738b6c38
ashutosh <amxdp18@gmail.com>
2018-08-01 23:02:15 +0530

    initial commit

---
+++ src/Ds.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Ds.cpp
+// Author      : Ashutosh
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Ashutosh!!" << endl; // prints !!!Hello Ashutosh!!
+	return 0;
+}
